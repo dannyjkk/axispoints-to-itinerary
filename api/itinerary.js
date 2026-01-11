0da8d80c1e609c6945d85a01587189da020d17e4
Danny <danny@users.noreply.github.com>
2026-01-11 15:19:02 +0530

    Use nodejs18.x runtime in vercel config

--- api/itinerary.js
+++ api/itinerary.js
@@ -49,7 +49,7 @@ function generateItinerary({ origin, destination, points = 0, days = 3 }) {
 }
 
 // ---- Vercel Serverless Handler ----
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const url = new URL(req.url, `https://${req.headers.host}`);
     const path = url.pathname;
